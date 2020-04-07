143ad7f6552c880eb4dcf5e7f7640d317fe85c70
xuwenchan <2696578102@qq.com>
2020-04-07 19:53:06 +0800

    添加rocketmq启动说明

---
+++ cloudParent/cloudRocketmq/src/main/java/com/rocketmq/application/RocketmqApplication.java
@@ -0,0 +1,11 @@
+package com.rocketmq.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqApplication {
+
+    public static void main(String[] args){
+
+    }
+}
